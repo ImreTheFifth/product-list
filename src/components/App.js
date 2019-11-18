79f8ba34aef47aa30b377126d208d0d3fb9a672f
ImreTheFifth <vidaimre88@gmail.com>
2019-11-18 15:27:19 +0100

    Change structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/App.css';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        /*this.state = {
+            
+        }*/
+    }
+    render() {
+        return (
+            <a href='https://www.index.hu' target="_blank">Visit index.hu </a>
+        );
+    }
+}
+
+export default App;
